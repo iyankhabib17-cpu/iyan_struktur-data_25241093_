f6aeff9b2dc747e2ccac5d326ef439747978272b
iyankhabib17-cpu <iyankhabib17@gmail.com>
2026-04-18 21:52:17 +0800

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,11 @@
+# perulangan (LOOP)
+# for kondisi :
+
+angka = -10
+
+while angka < 10 :
+    angka = int(input("masukan angka:"))
+    if angka < 10:
+        print("harus positif")
+
+print(f"angka di terima: {angka}")
