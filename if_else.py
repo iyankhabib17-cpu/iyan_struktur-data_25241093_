7c762d86019fbb60f8031be4981976740e01562b
iyankhabib17-cpu <iyankhabib17@gmail.com>
2026-04-18 21:50:34 +0800

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,15 @@
+# if
+# else
+#elif
+
+nama = input("masukan nama")
+NIM = input("masukan PIN NIM")
+
+if nama =="iyan si manis" :
+    NIM =="10"
+    print("HAI saya iyan si manis")
+    print("NIM saya 25241093")
+else:
+    print("mohon maaf anda bukan iyan si manis")
+    print("NIM anda tidak bisa diakses")
+print("program selesai")
